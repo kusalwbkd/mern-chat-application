3b12034a4753e7caf46aa353cc5fd8631e174140
kuslwbkd <kusalwbkddhananjaya@gmail.com>
2024-07-16 20:26:36 +0530

    socket url changed

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -18,7 +18,7 @@ export const SocketContextProvider=({children})=>{
 
     useEffect(()=>{
         if(authUser){
-         const socket=io("https://mern-chat-app-e1wh.onrender.com",{
+         const socket=io("https://mern-chat-application-u5gh.onrender.com",{
             query:{
                 userId:authUser._id
             }
